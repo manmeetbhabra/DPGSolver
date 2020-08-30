e74a4f5eed3f529d55538a860b6c24a2328da3a9
manmeetbhabra <manmeets.bhabra@gmail.com>
2020-08-30 16:28:15 -0400

    parameters_example.sh file updated to show how to include paths needed to make sure NLPQLP optimization cases work on this branch. Originally passing tests are still passing (92% pass rate in build and old tests that are expected to fail are still failing which is fine). Optimization case tested and working with new framework as well.

--- cmake/run/parameters_example.sh
+++ cmake/run/parameters_example.sh
@@ -5,3 +5,12 @@ export MKLROOT=/home/philip/software/mkl/mkl_2018_2/mkl
 export PETSC_DIR=/home/philip/software/petsc/petsc-3.9.2
 export PETSC_ARCH=arch-macOS-mkl-mpich-debug
 export SLEPC_DIR=/home/pzwan/software/slepc/slepc-3.9.1
+
+# Specific paths for libraries needed for optimization. 
+# This will be needed to run any optimization cases on the IGA_Manmeet branch.
+# Here, specify the path to the gfortran library file (should be in the gcc directory)
+# and the NLPQLP directory holding the library with the NLPQLP wrapper function.
+# Note that the NLPQLP library files may need to be rebuilt, in which case simply
+# enter that directory and use the Makefile.
+export LIBGFORTRAN_DIR="/usr/local/Cellar/gcc/10.2.0/lib/gcc/10"
+export LIBNLPQLP_DIR="/Users/jm-034232/Documents/McGill/Research/DPGSolver/NLPQLP_Optimizer"  
